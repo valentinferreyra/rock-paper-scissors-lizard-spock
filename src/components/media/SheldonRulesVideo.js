a78c9aa0ae574a9a6970824328a2c4cdac00da86
valentinferreyra <valen.ferreyra@outlook.com>
2022-07-11 12:23:15 -0300

    Diseño y estilo de seccion de reglas del juego terminados

--- src/components/media/SheldonRulesVideo.js
+++ src/components/media/SheldonRulesVideo.js
@@ -3,17 +3,19 @@ import { Link } from "react-router-dom";
 
 const SheldonRulesVideo = () => {
   return (
-    <div>
+    <div className="home-container">
       <iframe
-        width="560"
-        height="315"
+        width="1000"
+        height="615"
         src="https://www.youtube.com/embed/Y6GPW1vUaqU"
         title="YouTube video player"
         frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowfullscreen
       ></iframe>
-      <Link to="/">Volver</Link>
+      <div className="home-container__buttons">
+        <Link to="/">Volver a inicio</Link>
+      </div>
     </div>
   );
 };
--- src/page-components/Rules.js
+++ src/page-components/Rules.js
@@ -1,21 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import rules from "../img/rules.png";
 
 const Rules = () => {
   return (
-    <div>
-      <h1>Piedra, papel, tijera, lagarto, Spock</h1>
-      <p>Tijera corta a Papel </p>
-      <p>Papel tapa a Piedra </p>
-      <p>Piedra aplasta a Lagarto</p>
-      <p>Lagarto envenena a Spock </p>
-      <p>Spock rompe a Tijera </p>
-      <p>Tijera decapita a Lagarto</p>
-      <p>Lagarto devora a Papel </p>
-      <p>Papel desautoriza a Spock </p>
-      <p>Spock vaporiza a Piedra</p>
-      <p>y como siempre, Piedra aplasta a Tijera</p>
-      <div>
+    <div className="home-container">
+      <div className="home-container__rules">
+        <h1 className="home-container__rules-title">
+          Piedra, papel, tijera, lagarto, Spock
+        </h1>
+        <div className="home-container__rules-description">
+          <img src={rules} alt="rules" />
+          <div className="home-container__rules-description-text">
+            <p>Las reglas son simples...</p>
+            <p>Tijera corta a Papel </p>
+            <p>Papel tapa a Piedra </p>
+            <p>Piedra aplasta a Lagarto</p>
+            <p>Lagarto envenena a Spock </p>
+            <p>Spock rompe a Tijera </p>
+            <p>Tijera decapita a Lagarto</p>
+            <p>Lagarto devora a Papel </p>
+            <p>Papel desautoriza a Spock </p>
+            <p>Spock vaporiza a Piedra</p>
+            <p>y como siempre, Piedra aplasta a Tijera</p>
+          </div>
+        </div>
+      </div>
+      <div className="home-container__buttons">
         <Link to="/">Volver a inicio </Link>
         <Link to="/sheldonRules">O deja que Sheldon te lo explique </Link>
       </div>
